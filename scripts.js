e401e6e514cfb30dd64d5a8928927e1251e07bf8
Amanda-Quirino <aqrs@cin.ufpe.br>
2021-11-02 23:38:34 -0300

    portifolio Agradecimentos ao monitor Danilo Silva pela ajuda

---
+++ scripts.js
@@ -0,0 +1,103 @@
+function handleDiplomasDescription() {
+    document.getElementById(
+         'diplomas'
+    ).style.display='block';
+}
+
+function handleHideDiplomas() {
+    document.getElementById('diplomas').style.display='none'
+}
+
+function handleLinguagensDescription() {
+    document.getElementById(
+        'linguagens'
+    ).style.display='block';
+}
+
+function handleHideLinguagens() {
+    document.getElementById(
+        'linguagens'
+    ).style.display='none'
+}
+
+function handleInteressessDescription() {
+    document.getElementById(
+        'interesses'
+    ).style.display='block';
+}
+
+function handleHideInteresses() {
+    document.getElementById(
+        'interesses'
+    ).style.display='none'
+}
+
+function handleLinguagensDescription() {
+    document.getElementById(
+        'linguagens'
+    ).style.display='block';
+}
+
+function handleHideLinguagens() {
+    document.getElementById('linguagens').style.display='none'
+}
+
+function handleCuriosidades1Description() {
+    document.getElementById(
+        'curiosidade1'
+    ).style.display='block'
+}
+
+function handleHideCuriosidades1() {
+    document.getElementById(
+        'curiosidade1'
+    ).style.display='none'
+}
+
+function handleCuriosidades2Description() {
+    document.getElementById(
+        'curiosidade2'
+    ).style.display='block'
+}
+
+function handleHideCuriosidades2() {
+    document.getElementById(
+        'curiosidade2'
+    ).style.display='none'
+}
+
+function handleCuriosidades3Description() {
+    document.getElementById(
+        'curiosidade3'
+    ).style.display='block'
+}
+
+function handleHideCuriosidades3() {
+    document.getElementById(
+        'curiosidade3'
+    ).style.display='none'
+}
+
+function handleCuriosidades4Description() {
+    document.getElementById(
+        'curiosidade4'
+    ).style.display='block'
+}
+
+function handleHideCuriosidades4() {
+    document.getElementById(
+        'curiosidade4'
+    ).style.display='none'
+}
+
+function handleCuriosidades5Description() {
+    document.getElementById(
+        'curiosidade5'
+    ).style.display='block'
+}
+
+function handleHideCuriosidades5() {
+    document.getElementById(
+        'curiosidade5'
+    ).style.display='none'
+}
